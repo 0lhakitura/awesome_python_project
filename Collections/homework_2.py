b0f6605e43dcbd43d90bfdf90cf7de5021206626
0lhakitura <okitura96@gmail.com>
2023-09-12 00:08:10 +0300

    Made corrections to Homework 2.

--- Collections/homework_2.py
+++ Collections/homework_2.py
@@ -17,30 +17,44 @@ def generate_random_dicts():
     return list_of_dicts  # return the contents of the 'list_of_dicts' variable as its result
 
 
+def key_exists(d, target_key):
+    return target_key is not None and any(key.startswith(target_key) for key in d.keys())
+
+
+def select_keys_with_prefix(common_dict, prefix):
+    for key, value in common_dict.items():
+        if key.startswith(prefix):
+            return key
+
+
 def create_common_dict(list_of_dicts):
     common_dict = {}
-    list_of_list_of_keys = [list(d.keys()) for d in list_of_dicts]
+    indexed_dict = {}
     for i, dict in enumerate(list_of_dicts):
         for key, value in dict.items():
-            if key in common_dict:
-                if value > common_dict[key]:
+            if key_exists(common_dict, select_keys_with_prefix(common_dict, key)):
+                if value >= common_dict[select_keys_with_prefix(common_dict, key)]:
                     common_dict[f"{key}_{i + 1}"] = value
-                    del common_dict[key]
-                else:
-                    found_key = False
-                    for j, key_list in enumerate(list_of_list_of_keys):
-                        if key in key_list and not found_key:
-                            common_dict[f"{key}_{j + 1}"] = common_dict[key]
-                            found_key = True
-                            del common_dict[key]
+                    del common_dict[select_keys_with_prefix(common_dict, key)]
+                elif key in common_dict and value < common_dict[key]:
+                    key_lengths = {key: len(key) for key in common_dict.keys()}
+                    for key_kl, length_kl in key_lengths.items():
+                        if key_kl == key and length_kl == 1:
+                            for key_id, value_id in indexed_dict.items():
+                                if key_id == key:
+                                    common_dict[f"{key}_{value_id}"] = common_dict[key]
+                                    del common_dict[key]
+
             else:
                 common_dict[key] = value
+                indexed_dict[key] = i+1
     return common_dict
 
 
 def main():
     # random_dicts = generate_random_dicts()
-    random_dicts = [{'a': 2, 'b': 75, 'c': 40, 'j': 87}, {'a': 27, 'b': 9, 'c': 40, 'd': 87}]
+    random_dicts = [{'a': 27, 'b': 75, 'c': 40, 'j': 87}, {'a': 27, 'b': 9, 'c': 40, 'd': 87},
+                    {'a': 27, 'b': 9, 'c': 45, 'd': 87}]
     print("List of random dicts:")
     for i, dict in enumerate(random_dicts, start=1):
         print(f"Dict {i}: {dict}")
